abaa727faca6c8312d1ecae442d9db32a5cd8824
rockchip <someone@rock-chips.com>
2011-11-22 00:00:00 +0000

    rk29: config

---
+++ core/java/com/rockchip/android/macro/rkMacro.java
@@ -0,0 +1,14 @@
+package com.rockchip.android.macro;
+
+public class rkMacro {
+    final public static boolean BUTTON_ON_STATUS_BAR = false;
+    final public static boolean COMPATIBILITY_MODE_ON = false;
+    final public static boolean ENABLE_ETHERNET = false;
+    final public static boolean ENABLE_HDMI = false;
+    final public static boolean ENABLE_TVOUT = false;
+    final public static boolean FORCE_STATUSBAR = false;
+    final public static boolean MOUSE_INPUT = false;
+    final public static boolean NET_TRAFFIC_SERVICE_ON = false;
+    final public static boolean PHONE_NETWORK_CDMA = false;
+    final public static boolean SHOW_PHONE_SIGNAL_ON = false;
+}
