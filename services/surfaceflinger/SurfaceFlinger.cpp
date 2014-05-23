ahmedammar/platform_frameworks_base

ENGR00156872,Disable GPU activity during video playback Apply the following patch for video playback power on R11-rc3
commit 8dcbffd18f26e56e68b645c827e70f4a5a76cf49
Author: Zhou, Jie <b30303@freescale.com>
Date: Fri Apr 1 18:50:33 2011 +0800

ENGR00141724 Avoid enable GPU when play video

Only call glFinish and post fb when actual repaint is done

Signed-off-by: Zhou, Jie <b30303@freescale.com>
Signed-off-by: xianzhong <b07117@freescale.com>

services/surfaceflinger/SurfaceFlinger.cpp
@@ -434,12 +434,16 @@ bool SurfaceFlinger::threadLoop()
         logger.log(GraphicLog::SF_REPAINT, index);
         handleRepaint();
 
-        // inform the h/w that we're done compositing
-        logger.log(GraphicLog::SF_COMPOSITION_COMPLETE, index);
-        hw.compositionComplete();
+        // call glFinish and postfb only when actual repaint is done
+        if (!mSwapRegion.isEmpty()) {
+            // inform the h/w that we're done compositing
+            logger.log(GraphicLog::SF_COMPOSITION_COMPLETE, index);
+            hw.compositionComplete();
+            // release the clients before we flip ('cause flip might block)
 
-        logger.log(GraphicLog::SF_SWAP_BUFFERS, index);
-        postFramebuffer();
+            logger.log(GraphicLog::SF_SWAP_BUFFERS, index);
+            postFramebuffer();
+        }
 
         debugShowFPS();
         logger.log(GraphicLog::SF_REPAINT_DONE, index);

msink/android_frameworks_base

services/surfaceflinger/SurfaceFlinger.cpp
@@ -440,12 +440,16 @@ bool SurfaceFlinger::threadLoop()
         logger.log(GraphicLog::SF_REPAINT, index);
         handleRepaint();
 
-        // inform the h/w that we're done compositing
-        logger.log(GraphicLog::SF_COMPOSITION_COMPLETE, index);
-        hw.compositionComplete();
+	// call glFinish and postfb only when actual repaint is done
+	if (!mInvalidRegion.isEmpty()) {
+            // inform the h/w that we're done compositing
+            logger.log(GraphicLog::SF_COMPOSITION_COMPLETE, index);
+            hw.compositionComplete();
+            // release the clients before we flip ('cause flip might block)
 
-        logger.log(GraphicLog::SF_SWAP_BUFFERS, index);
-        postFramebuffer();
+            logger.log(GraphicLog::SF_SWAP_BUFFERS, index);
+            postFramebuffer();
+	}
 
         logger.log(GraphicLog::SF_REPAINT_DONE, index);
     } else {